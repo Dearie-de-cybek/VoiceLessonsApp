293c13f83e6f239c2c30ee7d324ba82f975d0832
Dearie <eburujacintha@gmail.com>
2024-07-14 03:05:38 +0100

    added darkmode context

--- voice-lesson-frontend/src/App.jsx
+++ voice-lesson-frontend/src/App.jsx
@@ -1,24 +1,60 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
-import GlobalStyles from './styles/GlobalStyles';
-import Login from "./pages/Login";
+import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
-// import AppLayout from "./ui/AppLayout";
+import Login from "./pages/Login";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+});
 
-
-const App = () => {
+function App() {
   return (
-    <>
-    <GlobalStyles />
-    <BrowserRouter>
-    <Routes>
-    <Route index element={<Navigate replace to="dashboard" />} />
-    <Route path="dashboard" element={<Dashboard />} />
-    <Route path="login" element={<Login />} />
-    </Routes>
-    </BrowserRouter>
-    </>    
-  )
+    <DarkModeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-0)",
+              color: "var(--color-grey-700)",
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </DarkModeProvider>
+  );
 }
-export default App
+
+export default App;
