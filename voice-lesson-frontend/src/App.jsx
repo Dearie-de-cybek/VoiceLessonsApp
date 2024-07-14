1037bda1b23d936cbb7f7b749b47e7726c1594c4
Dearie <eburujacintha@gmail.com>
2024-07-14 02:02:08 +0100

    configured routes for login and dashboard

--- voice-lesson-frontend/src/App.jsx
+++ voice-lesson-frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import styled from 'styled-components';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import GlobalStyles from './styles/GlobalStyles';
-import Button from "./ui/Button";
-import Heading from './ui/Heading';
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
-const StyledApp = styled.div`
-
-`;
 
 
 const App = () => {
   return (
     <>
     <GlobalStyles />
-    <StyledApp>
-      <Heading as="h1">Voice Lesson App For Abolo</Heading>
-      <Button onClick={() => alert("Buy my course")}>Abolo</Button>
-    </StyledApp>
+    <BrowserRouter>
+    <Routes>
+    <Route index element={<Navigate replace to="dashboard" />} />
+    <Route path="dashboard" element={<Dashboard />} />
+    <Route path="login" element={<Login />} />
+    </Routes>
+    </BrowserRouter>
     </>    
   )
 }
