e420f5ca88c67442eb74f948b3d54f9648bf45b3
Dearie <eburujacintha@gmail.com>
2024-07-14 02:56:36 +0100

    removed dashboard bug

--- voice-lesson-frontend/src/features/dashboard/DashboardLayout.jsx
+++ voice-lesson-frontend/src/features/dashboard/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Spinner from "../../ui/Spinner";
+// import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -9,23 +9,13 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
-  const { bookings, isLoading: isLoading1 } = useRecentBookings();
-  const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
-  const { cabins, isLoading: isLoading3 } = useCabins();
+ 
 
-  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+  
 
   return (
     <StyledDashboardLayout>
-      <Stats
-        bookings={bookings}
-        confirmedStays={confirmedStays}
-        numDays={numDays}
-        cabinCount={cabins.length}
-      />
-      <TodayActivity />
-      <DurationChart confirmedStays={confirmedStays} />
-      <SalesChart bookings={bookings} numDays={numDays} />
+      
     </StyledDashboardLayout>
   );
 }
